de64cda9ca63842bd3949abaf7d6759e30da9b08
Djonathan Quadras <djonquadras@gmail.com>
2022-04-08 10:47:35 -0300

    RL Classes.

--- Tutorials/TKinter.py
+++ Tutorials/TKinter.py
@@ -1,5 +1,7 @@
-import tkinter
+import gym
+#import numpy as np
+#from IPython import display
+#from matplotlib import pyplot as plt
 
-window = tkinter.Tk()
-
-window.mainloop()
+#from envs import Maze
+#%matplotlib inline
--- Tutorials/Teste.R
+++
@@ -1,3 +0,0 @@
-install.packages("bibliometrix")
-library(bibliometrix)
-biblioshiny()
