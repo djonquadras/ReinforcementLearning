4911beaee5c59fbcf3217be453bd1086d7e31a7a
Djonathan Quadras <djonquadras@gmail.com>
2022-04-21 11:55:40 -0300

    Remocao dos arquivos das aulas AIOM

---
+++ Tutorials/Simpy.py
@@ -0,0 +1,19 @@
+import simpy 
+
+# Create the environment
+env = simpy.Environment()
+
+# Define the process
+
+def car(env):
+    while True:
+        print('Start parking at %d' % env.now)
+        parking_duration = 5
+        yield env.timeout(parking_duration)
+        
+        print('Start driving at %d' % env.now)
+        trip_duration = 2
+        yield env.timeout(trip_duration)
+
+env.process(car(env))
+env.run(until=15)
--- Tutorials/TKinter.py
+++
@@ -1,7 +0,0 @@
-import gym
-#import numpy as np
-#from IPython import display
-#from matplotlib import pyplot as plt
-
-#from envs import Maze
-#%matplotlib inline
